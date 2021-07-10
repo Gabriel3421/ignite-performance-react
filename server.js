23e674cb57641951f646b703ca1f6372451cdace
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-07-10 11:02:43 -0300

    arrumando nome da variavel

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 module.exports = () => {
   const data = {
-    producs: [],
+    products: [],
   }
   for (let i = 0; i < 1000; i++) {
-    data.producs.push({
+    data.products.push({
       id: i + 1,
       price: (Math.random() + 1) * 10,
       title: `Camisera ${i + 1}`
