a1112383eb6b1f16538fa7759392ef62583996f4
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-07-10 11:03:53 -0300

    corrigindo erros de digitação

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ module.exports = () => {
   for (let i = 0; i < 1000; i++) {
     data.products.push({
       id: i + 1,
-      price: (Math.random() + 1) * 10,
-      title: `Camisera ${i + 1}`
+      price: Math.ceil((Math.random() + 1) * 10),
+      title: `Camiseta ${i + 1}`
     })
   }
   return data;
